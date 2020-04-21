c45679bbd9882e152610ed10242c685ad82495aa
kkjavatutorials <kkjavatutorials@gmail.com>
2020-04-21 20:47:41 +0530

    singleton design pattern code

---
+++ src/com/kkjavatutorials/singleton/Singleton.java
@@ -0,0 +1,15 @@
+package com.kkjavatutorials.singleton;
+
+public class Singleton {
+
+	private static final Singleton INSTANCE = new Singleton();
+	
+	private Singleton() {
+		
+	}
+	
+	public static Singleton getInstance() {
+		return INSTANCE;
+		
+	}
+}
